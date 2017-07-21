89d9ad1d46f1bf3061200597f96f892a4d31f018
github username <github email address>
2017-07-21 13:19:10 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Artist
+
+	attr_accessor :name
+	attr_reader :songs
+
+	@@song_count = 0
+
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
+
+	def add_song(song)
+		song.artist = self
+		@songs << song
+		@@song_count+=1
+	end
+
+	def add_song_by_name(name)
+		name = Song.new(name)
+		add_song(name)
+	end
+
+	def self.song_count
+		@@song_count
+	end
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Author
+
+	attr_accessor :name
+	attr_reader :posts
+
+	@@post_count = 0
+
+	def initialize(name)
+		@name = name
+		@posts = []
+	end
+
+	def add_post(post)
+		post.author = self
+		@posts << post
+		@@post_count +=1
+	end
+
+	def add_post_by_title(title)
+		title = Post.new(title)
+		add_post(title)
+	end
+
+	def self.post_count
+		@@post_count
+	end
+end
